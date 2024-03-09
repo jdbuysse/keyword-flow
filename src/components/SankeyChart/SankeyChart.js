4b9d1c4ca86e2e253b4904bc2339a27dcedee733
jdbuysse <jdbuysse@gmail.com>
2024-03-09 11:39:04 -0700

    sankey concept, pre-refactor

---
+++ src/components/SankeyChart/SankeyChart.js
@@ -0,0 +1,54 @@
+import React from "react";
+import { ResponsiveSankey } from "@nivo/sankey";
+
+const SankeyDiagram = ({ data }) => {
+  return (
+    <div style={{ height: "500px" }}>
+      <ResponsiveSankey
+        data={data}
+        margin={{ top: 40, right: 160, bottom: 40, left: 50 }}
+        align="justify"
+        colors={{ scheme: "category10" }}
+        nodeOpacity={1}
+        nodeThickness={18}
+        nodeInnerPadding={3}
+        nodeSpacing={24}
+        nodeBorderWidth={0}
+        nodeBorderColor={{ from: "color", modifiers: [["darker", 0.8]] }}
+        linkOpacity={0.5}
+        linkHoverOthersOpacity={0.1}
+        enableLinkGradient={true}
+        labelPosition="outside"
+        labelOrientation="horizontal"
+        labelPadding={16}
+        labelTextColor={{ from: "color", modifiers: [["darker", 1]] }}
+        animate={true}
+        motionStiffness={140}
+        motionDamping={13}
+        legends={[
+          {
+            anchor: "bottom-right",
+            direction: "column",
+            translateX: 130,
+            itemWidth: 100,
+            itemHeight: 14,
+            itemDirection: "right-to-left",
+            itemsSpacing: 2,
+            itemTextColor: "#999",
+            symbolSize: 14,
+            effects: [
+              {
+                on: "hover",
+                style: {
+                  itemTextColor: "#000",
+                },
+              },
+            ],
+          },
+        ]}
+      />
+    </div>
+  );
+};
+
+export default SankeyDiagram;
